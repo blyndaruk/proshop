bc1a105c44badb37751461b5f2fb338bc26c5cb6
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2022-02-20 16:00:38 +0200

    add dummy seeder and router

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    // {
-    //   useUnifiedTopology: true,
-    //   useNewUrlParser: true,
-    //   useCreateIndex: true,
-    // }
-    const connect = await mongoose.connect(`mongodb://${process.env.HOST}:${process.env.DB_PORT}`)
-    console.log(`MongoDB connected: ${connect.connection.host} on port ${process.env.DB_PORT}`);
+    await mongoose.connect(`mongodb://${process.env.HOST}:${process.env.DB_PORT}`, {
+      dbName: process.env.DB_NAME,
+      auth: {
+        password: process.env.DB_USER_PASSWORD,
+        username: process.env.DB_USER_NAME,
+      },
+    })
+    console.log(`MongoDB connected: ${process.env.HOST} on port ${process.env.DB_PORT}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
     process.exit(1)
